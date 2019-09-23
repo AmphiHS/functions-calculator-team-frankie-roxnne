70e1a74eeeb2a85d9cabf8aec8c6bdd9c3ec45c4
frankie <ec2-user@ip-172-31-85-205.ec2.internal>
2019-09-23 19:02:25 +0000

    added the slope Formula

--- partner-1.js
+++ partner-1.js
@@ -3,8 +3,12 @@ console.log ("Script Loaded!");
 
 const MiKil = (num1) => {
   return   num1 * 1.609;
-}
+};
 
 const PouKil = (num2) => {
     return num2 * 0.45;
-}
+};
+
+const SloFor = (x1, y1, x2, y2) => {
+  return (y2 -y1)/(x2 -x1);
+};
